374444732ef67194bbc55c4a328f1227d9d39263
Rachael <rachael.ellen@btopenworld.com>
2018-06-18 20:50:06 +0100

    added base map toggle widget and some test points

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ class App extends PureComponent {
 
   constructor(props){
     super(props)
-    this.state = {map: null}
+    this.coords = {
+      testCoords: [
+        [-2.68, 55.97],
+        [-3.94, 56.12],
+        [-3.51, 56.00]
+      ]
+    };
+    // this.state = {map: null}
   }
 
   render() {
@@ -19,21 +26,91 @@ class App extends PureComponent {
       <div className ="App">
         <EsriLoaderReact
           options={options}
-          modulesToLoad={['esri/Map', 'esri/views/MapView']}
-          onReady={({loadedModules: [Map, MapView], containerNode}) => {
-            this.setState({
-              view: new MapView({
-                container: containerNode,
-                center: [-3.2, 55.5],
-                zoom: 6,
-                map: new Map({basemap: 'satellite'})
-              })
-            })
+          modulesToLoad={['esri/Map', 'esri/views/MapView', 'esri/Graphic', 'esri/widgets/BasemapToggle']}
+          onReady={({loadedModules: [Map, MapView, Graphic, BasemapToggle], containerNode}) => {
+            // this.setState({
+
+            const map = new Map({
+              basemap: 'oceans'
+            });
+
+            const mapView = new MapView({
+              container: containerNode,
+              center: [-3.2, 55.5],
+              zoom: 6,
+              map: map
+            });
+
+            const polyline = {
+              type: "polyline",
+              paths: this.coords.testCoords
+            };
+
+            const pointAtt = {
+              LocalityName: "Place Holder For Name",
+              LocalityDescription: "Place Holder for Description",
+              DataHeld: "Links to data:"
+            };
+
+
+
+            const multiPoints = {
+              type: "multipoint",
+              points: [
+                [-2.68, 55.97],
+                [-3.94, 56.12],
+                [-3.51, 56.00],
+              ]
+            };
+
+            const markerSymbol = {
+              type: "simple-marker",
+              color: [226, 119, 40],
+              outline: {
+                color: [255, 255, 255],
+                width: 2
+              }
+            };
+
+            const pointGraphic = new Graphic({
+              geometry: multiPoints,
+              symbol: markerSymbol,
+              attributes: pointAtt,
+              popupTemplate: {
+                title: "{LocalityName}",
+                content: [{
+                  type: "fields",
+                  fieldInfos: [{
+                    fieldName: "LocalityName"
+                  }, {
+                    fieldName: "LocalityDescription"
+                  }, {
+                    fieldName: "DataHeld"
+                  }]
+                }]
+              }
+            });
+
+            mapView.graphics.addMany([pointGraphic]);
+
+            const toggle = new BasemapToggle({
+              view: mapView,
+              nextBasemap: "satellite"
+            });
+
+            mapView.ui.add(toggle, "top-right");
+
+
+
+
+
+            // })
           }}
         />
       </div>
     );
   }
+
 }
 
 export default App;
