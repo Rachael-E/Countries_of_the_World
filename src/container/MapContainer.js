cda3a40249f80e8a6e665bd1bb9913f065ba317a
Rachael <rachael.ellen@btopenworld.com>
2018-06-19 16:51:10 +0100

    points now display unique information

--- src/container/MapContainer.js
+++ src/container/MapContainer.js
@@ -6,43 +6,36 @@ class MapContainer extends PureComponent {
   constructor(props){
     super(props)
     this.state = {
-      testCoords: null,
+      allCountryCoords: null,
       countries: [],
       currentCountry: null
     };
     // this.state = {map: null}
+    this.onReadyCallback = this.onReadyCallback.bind(this);
   }
 
   componentDidMount(){
     const url = 'https://restcountries.eu/rest/v2/all';
     fetch(url)
-      .then(res => res.json())
-      .then( (countriesData) => {
+    .then(res => res.json())
+    .then( (countriesData) => {
 
-          const allCountriesLatLng = countriesData.map((country) => {
-            return country.latlng.reverse()
-          });
+      const allCountriesLatLng = countriesData.map((country, index) => {
+        return country.latlng.reverse()
+      });
 
-          this.setState({
-            countries: countriesData,
-            testCoords: allCountriesLatLng
-          })
-        })
-      // .then(countriesData => console.log(countriesData))
-      .catch(error => console.log("Error:", error))
-      // console.log(this.state.countries)
+      this.setState({
+        countries: countriesData,
+        allCountryCoords: allCountriesLatLng
+      })
+    })
+    // .then(countriesData => console.log(countriesData))
+    .catch(error => console.log("Error:", error))
   }
 
-  render() {
+  onReadyCallback({loadedModules: [Map, MapView, Graphic, BasemapToggle], containerNode}){
 
-    const options = {
-      url: 'https://js.arcgis.com/4.7/'
-    };
-
-    const onReadyCallback = ({loadedModules: [Map, MapView, Graphic, BasemapToggle], containerNode}) => {
-    // this.setState({
-
-      console.log("this.state.testCoords", this.state.testCoords)
+      console.log("this.state.allCountryCoords", this.state.allCountryCoords)
 
       const theMap = new Map({
         basemap: 'oceans'
@@ -55,75 +48,72 @@ class MapContainer extends PureComponent {
         map: theMap
       });
 
-      // const polyline = {
-      //   type: "polyline",
-      //   paths: this.state.testCoords
-      // };
 
-      const pointAtt = {
-        LocalityName: "Place Holder For Name",
-        LocalityDescription: "Place Holder for Description",
-        DataHeld: "Links to data:"
-      };
+      const uniqueCountryMarkers = this.state.countries.map((country) => {
 
-      // const countryLat = this.state.countries[1].latlng[0];
-      // const countryLng = this.state.countries[1].latlng[1];
-      // const countryLngLat = [countryLng, countryLat];
-      // console.log(countryLngLat);
+        return new Graphic({
+          geometry: {
+            type: 'point',
+            longitude: country.latlng[0],
+            latitude: country.latlng[1]
+          },
+          symbol: {
+            type: "simple-marker",
+            color: [226, 119, 40],
+            outline: {
+              color: [255, 255, 255],
+              width: 2
+            }
+          },
+          attributes: {
+            LocalityName: country.name,
+            LocalityDescription: country.region,
+            DataHeld: "Links to data:"
+          },
+          popupTemplate: {
+            title: "{LocalityName}",
+            content: [
+              {
+                type: "fields",
+                fieldInfos: [
+                  {fieldName: "LocalityName"}, {fieldName: "LocalityDescription"}, {fieldName: "DataHeld"}
+                ]
+              }
+            ]
+          }
+        })
+      })
 
-      const multiPoints = {
-        type: "multipoint",
-        points: this.state.testCoords
-      };
 
-      const markerSymbol = {
-        type: "simple-marker",
-        color: [226, 119, 40],
-        outline: {
-          color: [255, 255, 255],
-          width: 2
-        }
-      };
-
-      const pointGraphic = new Graphic({
-        geometry: multiPoints,
-        symbol: markerSymbol,
-        attributes: pointAtt,
-        popupTemplate: {
-          title: "{LocalityName}",
-          content: [{
-            type: "fields",
-            fieldInfos: [{
-              fieldName: "LocalityName"
-            }, {
-              fieldName: "LocalityDescription"
-            }, {
-              fieldName: "DataHeld"
-            }]
-          }]
-        }
-      });
-      mapView.graphics.addMany([pointGraphic]);
+      mapView.graphics.addMany(uniqueCountryMarkers);
 
       const toggle = new BasemapToggle({
         view: mapView,
         nextBasemap: "satellite"
       });
       mapView.ui.add(toggle, "top-right");
+
     }
 
-    if ( !this.state.testCoords ) {
+
+  render() {
+
+    const options = {
+      url: 'https://js.arcgis.com/4.7/'
+    };
+
+    if ( !this.state.allCountryCoords ) {
       var componentToRender = (
         <p>Loading data ....</p>
       )
     } else {
-        var componentToRender = (
-            <EsriLoaderReact
-              options={options}
-              modulesToLoad={['esri/Map', 'esri/views/MapView', 'esri/Graphic', 'esri/widgets/BasemapToggle']}
-              onReady={onReadyCallback}
-            />
-          )
+      var componentToRender = (
+        <EsriLoaderReact
+          options={options}
+          modulesToLoad={['esri/Map', 'esri/views/MapView', 'esri/Graphic', 'esri/widgets/BasemapToggle']}
+          onReady={this.onReadyCallback}
+        />
+      )
     }
 
     return (
@@ -133,5 +123,6 @@ class MapContainer extends PureComponent {
     );
   }
 }
+
 
 export default MapContainer;
